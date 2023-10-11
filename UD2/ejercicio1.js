b55bae6c834b83efe3c93edfe77caa5c1d3eaa23
VicenteAlarcon <alarcongonzalezvicente@gmail.com>
2023-10-11 05:33:06 +0200

    feat: ejercicio2

--- UD2/ejercicio1.js
+++ UD2/ejercicio1.js
@@ -2,3 +2,4 @@ let parrafo = document.getElementById("parrafo")
 let pantalla = document
 
 pantalla.onmousemove = (event) => {parrafo.textContent = "Posición x: "+event.clientX+" Posición Y: "+event.clientY}
+
